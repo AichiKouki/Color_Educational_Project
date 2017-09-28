cf8373e88b8a936a97017c40818fe00a22be8825
愛知巧規 <prosenyou123@gmail.com>
2017-09-28 16:30:11 +0900

    RBGの値をUnity側で取得

---
+++ Assets/Application/Script/GetColorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetColorController : MonoBehaviour {
+
+	public SerialController serialController;//シリアル通信を行うスクリプト
+	public Text R_Label;
+	public Text G_Label;
+	public Text B_Label;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		R_Label.text = serialController.r;
+		G_Label.text = serialController.b;
+		B_Label.text = serialController.b;
+	}
+}
---
+++ Assets/Application/Script/SerialController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System;
+using System.IO.Ports;
+using UnityEngine;
+using UniRx;//UniRxを使うために、これを追加している
+
+public class SerialController : MonoBehaviour {
+
+	public string portName;
+	public int baurate;
+
+	SerialPort serial;
+	bool isLoop = true;
+
+	//他のスクリプトに値を取得してもらいたいから、そのための変数
+	public string[] RGB=new string[3];//RGBだから、三つ文初期化
+	public int r;
+	public int g;
+	public int b;
+
+	//Startメソッドの中でSerialクラスのインスタンスを作成した後、Scheduleメソッドを使ってReadDataスレッドを立ちあげています。
+	void Start () 
+	{
+		this.serial = new SerialPort (portName, baurate, Parity.None, 8, StopBits.One);
+
+		try
+		{
+			this.serial.Open();
+			Scheduler.ThreadPool.Schedule (() => ReadData ()).AddTo(this);
+		} 
+		catch(Exception e)
+		{
+			Debug.Log ("can not open serial port");
+		}
+	}
+
+	//ReadDataメソッドの中ではSerialクラスのReadLineメソッドを使って、シリアルで送られてくるデータを読んで表示しています。
+	public void ReadData()
+	{
+		while (this.isLoop)
+		{
+			string message = this.serial.ReadLine();
+			Debug.Log(message);//Arduino側の、printlnの部分を取得している
+			RGB=message.Split(',');//「,」で区切り文字で分割して配列に変換している
+			r = int.Parse (RGB[0]);//文字列で取得している部分をint型に変換して、rに代入
+			g = int.Parse (RGB[1]);
+			b = int.Parse (RGB[2]);
+		}
+	}
+
+	//ゲームを終了する場合はこのメソッドが実行される
+	void OnDestroy()
+	{
+		this.isLoop = false;
+		this.serial.Close ();
+	}
+}
