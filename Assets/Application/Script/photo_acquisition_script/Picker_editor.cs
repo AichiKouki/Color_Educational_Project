abb81651f164e6d68269d38ce191d75640ac5a7a
愛知巧規 <prosenyou123@gmail.com>
2017-09-29 17:05:15 +0900

    写真合成シーン作成終了

--- Assets/Application/Script/photo_acquisition_script/Picker_editor.cs
+++ Assets/Application/Script/photo_acquisition_script/Picker_editor.cs
@@ -17,10 +17,10 @@ namespace Kakera
 					File.Delete(destination);
 				File.Copy(path, destination);
 				Debug.Log ("PickerOSX:" + destination);
-				if (title == "Select Image") {
+				if (title == "Select Image") {//自分の編集部分。元のコードが、receiverにUnimgpickerとなっていた。二つの画像を読み込みたいので、Unimgpicker2も作成したので、使うスクリプトファイルによって条件分岐する
 					var receiver = GameObject.Find ("Unimgpicker");//ここが、画像を帰る空のオブジェクトを見つける処理
 					receiver.SendMessage("OnComplete", Application.persistentDataPath + "/" + outputFileName);
-				} else if (title == "Select Image2") {
+				} else if (title == "Select Image2") {//自分が追加した部分
 					var receiver = GameObject.Find ("Unimgpicker2");//ここが、画像を帰る空のオブジェクトを見つける処理
 					receiver.SendMessage("OnComplete", Application.persistentDataPath + "/" + outputFileName);
 				}
