1ab77f3562b7ac446c2cd3f98f75c019f4743687
愛知巧規 <prosenyou123@gmail.com>
2017-10-09 16:18:23 +0900

    線を描くシーンで色を8種類に増加

--- Assets/Application/Script/ChangeColorController.cs
+++ Assets/Application/Script/ChangeColorController.cs
@@ -24,16 +24,22 @@ public class ChangeColorController : MonoBehaviour {
 	//ボタンによって処理を分岐させる処理
 	void ChangeColor(string name){
 		if (name == "ChangeBlackButton") {//ボタンの名前がChangeBlackButtonだったら処理
-			drawLineController.changeColorNum=0;//色のオブジェクトの配列の添え字の部分の値を変更
-			Debug.Log("黒のボタンが押されました");
-		} else if (name == "ChangeBlueButton") {
-			drawLineController.changeColorNum=1;//色のオブジェクトの配列の添え字の部分の値を変更
-		} else if (name == "ChangeGreenButton") {
-			drawLineController.changeColorNum=2;//色のオブジェクトの配列の添え字の部分の値を変更
+			drawLineController.changeColorNum = 0;//色のオブジェクトの配列の添え字の部分の値を変更
+			Debug.Log ("黒のボタンが押されました");
+		} else if (name == "ChangeBlueButton") {//青
+			drawLineController.changeColorNum = 1;//色のオブジェクトの配列の添え字の部分の値を変更
+		} else if (name == "ChangeGreenButton") {//緑
+			drawLineController.changeColorNum = 2;//色のオブジェクトの配列の添え字の部分の値を変更
 		} else if (name == "ChangeRedButton") {
-			drawLineController.changeColorNum=3;//色のオブジェクトの配列の添え字の部分の値を変更
-		} else if (name == "ChangeYellowButton") {
-			drawLineController.changeColorNum=4;//色のオブジェクトの配列の添え字の部分の値を変更
+			drawLineController.changeColorNum = 3;//色のオブジェクトの配列の添え字の部分の値を変更
+		} else if (name == "ChangeYellowButton") {//あか
+			drawLineController.changeColorNum = 4;//色のオブジェクトの配列の添え字の部分の値を変更
+		} else if (name == "ChangeBrownButton") {//茶色
+			drawLineController.changeColorNum = 5;//色のオブジェクトの配列の添え字の部分の値を変更
+		} else if (name == "ChangePurpleButton") {//紫
+			drawLineController.changeColorNum = 6;
+		} else if (name == "ChangePinkButton") {//ピンク
+			drawLineController.changeColorNum = 7;
 		}
 	}
 }
---
+++ Assets/Application/Script/ColorPicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//canvasにアタッチする
+public class ColorPicker : MonoBehaviour {
+	GameObject canvas,picker;
+	float c(float f){  return f<0 ? 0 : f>256 ? 1 : f/256f; }
+	void Start () {
+		canvas  = GameObject.Find("Canvas"); //キャンバス
+		picker  = new GameObject("Picker");  //メイン部分
+
+		int w = 256*3-1,h=256;
+		Texture2D tex = new Texture2D(w,h);
+		float r,g,b,ex;
+		for(int i=0;i<w;i++)for(int j=0;j<h;j++){
+				r = Mathf.Max(256-i,i-512,0);
+				g = i<256 ? i : Mathf.Max(512-i,0);
+				b = i<512 ? Mathf.Max(i-256,0) : 768-i;
+				ex= j-h/2;
+				tex.SetPixel(i,j,new Color(c(r+ex) , c(g+ex) , c(b+ex) , 1));
+			}
+		tex.Apply();
+
+		// ピッカーの設定
+		picker.transform.parent = canvas.transform;
+		Image img = picker.AddComponent<Image>();
+		picker.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+		picker.GetComponent<RectTransform>().sizeDelta = new Vector2(1024,1024);
+		img.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
+	}
+}
