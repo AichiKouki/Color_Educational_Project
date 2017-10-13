932b1555f77d686b019c03a68e950d6c3526bf78
愛知巧規 <prosenyou123@gmail.com>
2017-10-13 11:36:40 +0900

    [WIP]カラーピッカー表示

--- Assets/Application/Script/ColorPicker.cs
+++ Assets/Application/Script/ColorPicker.cs
@@ -1,33 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
+using System;//Convertを使うからインポートする
+using System.Collections;
 
-//canvasにアタッチする
 public class ColorPicker : MonoBehaviour {
-	GameObject canvas,picker;
-	float c(float f){  return f<0 ? 0 : f>256 ? 1 : f/256f; }
-	void Start () {
-		canvas  = GameObject.Find("Canvas"); //キャンバス
-		picker  = new GameObject("Picker");  //メイン部分
+	public Texture2D colorPicker;
+	public int ImageWidth = 100;
+	public int ImageHeight = 100;
 
-		int w = 256*3-1,h=256;
-		Texture2D tex = new Texture2D(w,h);
-		float r,g,b,ex;
-		for(int i=0;i<w;i++)for(int j=0;j<h;j++){
-				r = Mathf.Max(256-i,i-512,0);
-				g = i<256 ? i : Mathf.Max(512-i,0);
-				b = i<512 ? Mathf.Max(i-256,0) : 768-i;
-				ex= j-h/2;
-				tex.SetPixel(i,j,new Color(c(r+ex) , c(g+ex) , c(b+ex) , 1));
-			}
-		tex.Apply();
+	void OnGUI(){
+		//ここで、ポジションとサイズを設定している
+		if (GUI.RepeatButton(new Rect(500, 500, ImageWidth, ImageHeight), colorPicker)) {
+			Vector2 pickpos = Event.current.mousePosition;
+			//文字から数値に変換
+			//int aaa = Convert.ToInt32(pickpos.x);
+			//int bbb = Convert.ToInt32(pickpos.y);
+			int aaa=Convert.ToInt64(pickpos.x);
+			int bbb=Convert.ToInt64(pickpos.y);
+			Color col = colorPicker.GetPixel(aaa,41-bbb);
 
-		// ピッカーの設定
-		picker.transform.parent = canvas.transform;
-		Image img = picker.AddComponent<Image>();
-		picker.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-		picker.GetComponent<RectTransform>().sizeDelta = new Vector2(1024,1024);
-		img.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
+			Debug.Log(col);
+		}
 	}
 }
