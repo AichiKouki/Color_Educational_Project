5483241800c95e81af653769963af14148dc40bb
愛知巧規 <prosenyou123@gmail.com>
2017-10-31 19:02:37 +0900

    スクリプトからではRGBの最高値が1と知り、mainTextureをやめるためにRawImageに変更

--- Assets/Application/Script/UseCamera/CameraController.cs
+++ Assets/Application/Script/UseCamera/CameraController.cs
@@ -6,15 +6,17 @@ using UnityEngine.UI;
 public class CameraController : MonoBehaviour  {
 	public WebCamTexture webCameraTexture = null;
 	public GameObject plane;
+	//[SerializeField]
+	//Image image;
 	[SerializeField]
-	Image image;
+	RawImage camera_videos_Image;
 
 	// Use this for initialization
 	void Start ()
 	{
 		webCameraTexture = new WebCamTexture ();
-		//plane.GetComponent<Renderer>().material.mainTexture = webCameraTexture;
-		image.material.mainTexture = webCameraTexture;
+		//image.material.mainTexture = webCameraTexture;//mainTextureでやってしまうと、他のImageにも同じ映像が表示されてしまう
+		camera_videos_Image.texture=webCameraTexture;
 		webCameraTexture.Play ();
 	}
 
--- Assets/Application/Script/UseCamera/GetColor.cs
+++ Assets/Application/Script/UseCamera/GetColor.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+//Texture2D.ReadPixelsで画面のピクセルが取得できるので、その１ドットだけ取得して色を抽出している。
 public class GetColor : MonoBehaviour
 {
 	public Color color;
 	private Texture2D tex = null;
+
+	[SerializeField]
+	Image testImage;
 
 	void Start()
 	{
@@ -15,19 +18,22 @@ public class GetColor : MonoBehaviour
 	}
 
 	void Update(){
-		//Debug.Log (color);
+		Debug.Log ("R="+color.r*255+"G="+color.g*255+"B="+color.b*255);//インスペクタからの場合RGBの最高値は255だが、スクリプトの場合最高値が1である。だから、スクリプトから取得する場合255かける必要がある。
+
+		testImage.color = new Color(color.r*255/255,color.g*255/255,color.b*255/255,255/255);
+		//testImage.color=new Color(255/255,0/255,0/255,255/255);
 	}
 
 	IEnumerator OnPostRender(){
 		while (true) {
 			//レンダリングが完了するまで待つ
-			yield return new WaitForEndOfFrame();
+			yield return new WaitForEndOfFrame();//これをしないとエラーがでる
 			Vector2 pos = Input.mousePosition ;
 			//tex.ReadPixels(new Rect(pos.x, pos.y, 1, 1), 0, 0);
 			tex.ReadPixels(new Rect(670, 377, 1, 1), 0, 0);//(670,377)がiPhone 6Sの真ん中の座標となる
 			//tex.ReadPixels(new Rect(screenToWorldPointPosition.x, screenToWorldPointPosition.y, 1, 1), 0, 0);
 			color = tex.GetPixel(0,0);
-			Debug.Log (pos);
+			//Debug.Log (pos);
 		}
 	}
 }
